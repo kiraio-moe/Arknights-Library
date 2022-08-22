268ba6d9bd626d28b2c443645192b99b2d87a45c
Bayu Satiyo <itsyuukunz@gmail.com>
2022-08-22 10:44:49 +0700

    Add beta design of the page
    Signed-off-by: Bayu Satiyo <itsyuukunz@gmail.com>

---
+++ _drafts/Arknights Wiki - GamePress.md
@@ -0,0 +1,5 @@
+# [Arknights Wiki - GamePress](https://gamepress.gg/arknights)
+
+A complete Arknights wiki's.
+
+Here you can find anything about Arknights. Start from Guides for both New and Expert Doctor, Tools to utilize your resources and maximize your experinces with Arknights, Latest news about Arknights, Reading Arknights lore's or browse your Waifu images at Gallery. If you just want to chill out, go checkout their official GamePress community and Discord server.
---
+++ assets/js/main.js
@@ -0,0 +1,92 @@
+$(() => {
+    /*** Sticky section ***/
+    // height = visible window
+    $viewportHeight = window.innerHeight;
+    const navbarLink = $(
+        "#offcanvasTop > div > div.offcanvas-body > nav > a.nav-link"
+    );
+
+    $(".vh").css({
+        height: $viewportHeight,
+    });
+
+    $.scrollify({
+        section: ".sticky-section",
+        easing: "easeOutExpo",
+        interstitialSection: ".footer",
+        scrollSpeed: 150,
+        offset: 0,
+        scrollbars: true,
+        standardScrollElements: "",
+        setHeights: true,
+        overflowScroll: true,
+        updateHash: false,
+        touchScroll: true,
+        before: function () {
+            // navbarLink.removeClass("active");
+        },
+        after: function () {
+            UpdateNavbarLinkState(navbarLink);
+        },
+        afterResize: function () {},
+        afterRender: function () {
+            UpdateNavbarLinkState(navbarLink);
+        },
+    });
+
+    $("#offcanvasTop > div > div.offcanvas-body > nav > a").click(function () {
+        $.scrollify.move("#" + $(this).attr("data-scrollify-move"));
+    });
+
+    /*** Hamburger menu ***/
+    const hamburger = $(".navbar-toggle > .hamburger");
+    hamburger.click(function () {
+        hamburger.toggleClass("is-active");
+        // Do something else, like open/close menu
+    });
+});
+
+function UpdateNavbarLinkState(obj) {
+    current = $.scrollify.current();
+    obj.removeClass("active");
+
+    for (var i = 0; i < obj.length; i++) {
+        var el = obj.eq(i);
+        if (
+            $(el).attr("data-scrollify-move") ==
+            current.attr("data-section-name")
+        ) {
+            el.addClass("active");
+        }
+    }
+}
+
+// VanillaTilt.init(document.querySelector(".header"), {
+//     max: 2,
+//     speed: 400,
+// });
+// , {
+//     reverse: false, // reverse the tilt direction
+//     max: 2, // max tilt rotation (degrees)
+//     startX: 0, // the starting tilt on the X axis, in degrees.
+//     startY: 0, // the starting tilt on the Y axis, in degrees.
+//     perspective: 1000, // Transform perspective, the lower the more extreme the tilt gets.
+//     scale: 1, // 2 = 200%, 1.5 = 150%, etc..
+//     speed: 300, // Speed of the enter/exit transition
+//     transition: true, // Set a transition on enter/exit.
+//     axis: null, // What axis should be disabled. Can be X or Y.
+//     reset: true, // If the tilt effect has to be reset on exit.
+//     easing: "cubic-bezier(.03,.98,.52,.99)", // Easing on enter/exit.
+//     glare: false, // if it should have a "glare" effect
+//     "max-glare": 1, // the maximum "glare" opacity (1 = 100%, 0.5 = 50%)
+//     "glare-prerender": false, // false = VanillaTilt creates the glare elements for you, otherwise
+//     // you need to add .js-tilt-glare>.js-tilt-glare-inner by yourself
+//     "mouse-event-element": null, // css-selector or link to an HTML-element that will be listening to mouse events
+//     "full-page-listening": true, // If true, parallax effect will listen to mouse move events on the whole document, not only the selected element
+//     gyroscope: true, // Boolean to enable/disable device orientation detection,
+//     gyroscopeMinAngleX: -45, // This is the bottom limit of the device angle on X axis, meaning that a device rotated at this angle would tilt the element as if the mouse was on the left border of the element;
+//     gyroscopeMaxAngleX: 45, // This is the top limit of the device angle on X axis, meaning that a device rotated at this angle would tilt the element as if the mouse was on the right border of the element;
+//     gyroscopeMinAngleY: -45, // This is the bottom limit of the device angle on Y axis, meaning that a device rotated at this angle would tilt the element as if the mouse was on the top border of the element;
+//     gyroscopeMaxAngleY: 45, // This is the top limit of the device angle on Y axis, meaning that a device rotated at this angle would tilt the element as if the mouse was on the bottom border of the element;
+//     gyroscopeSamples: 10, // How many gyroscope moves to decide the starting position.
+// });
